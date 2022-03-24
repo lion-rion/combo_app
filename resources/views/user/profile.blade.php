c7a96996c0d1a15ac13997fdee3e5e575503875f
Lion-lion <lionact08@gmail.com>
2022-03-24 22:31:00 +0900

    プロフページ追加

---
+++ resources/views/user/profile.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+@section('title','プロフィール') <!--タイトルはコンボのタイトルに設定するとイイかも？？-->
+@section('content')
+<div id="main_content_wrap">
+  <div id="content_wrap">
+        <!--左側メニューのラップ-->
+        <div class="right_menu_wrap">
+        </div>
+        <!--左側メニューのラップ終了-->
+        <!--中央メニューのラップ-->
+        <div class="post_content_wrap">
+            <section class="profile_section_wrap">
+                <div class="profile_wrap">
+                    <div class="profile_card">
+                        <img class="profile_img" src="{{ asset('storage/profiles/'.$user->profile_image) }}" id="img">
+                        <p class="profile_username"><span>@</span>{{ $user->name }}</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!--右側メニューのラップ-->
+        <div class="left_menu_wrap"></div>
+        <!--右側メニューのラップ終了-->
+  </div>
+</div>
+
+@endsection
