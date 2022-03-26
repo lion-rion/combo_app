d55ee9cf6982e7ef8387d14530693bea72674d0a
Lion-lion <lionact08@gmail.com>
2022-03-27 06:29:47 +0900

    migrationを統合

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('profile_image')->default('default.png');
+            $table->string('profile_text')->nullable();
             $table->timestamps();
         });
     }
